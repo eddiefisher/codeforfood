3faf78e9cba5006942b14bf8a4f3c366fe103dcd
pyromaniac <kinwizard@gmail.com>
2013-02-02 21:08:11 +0700

    Added database symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,16 @@ role :web, server_name                          # Your HTTP server, Apache/etc
 role :app, server_name                          # This may be the same as your `Web` server
 role :db, server_name, :primary => true # This is where Rails migrations will run
 
+namespace :customs do
+  task :config do
+    run "ln -nfs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  end
+end
+
+after 'deploy:update_code', 'customs:config'
+# after 'deploy:symlink', 'customs:symlink'
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# after 'deploy:restart', 'deploy:cleanup'
 
 # after 'deploy:restart', 'unicorn:reload'
 # after 'deploy:restart', 'unicorn:restart'
